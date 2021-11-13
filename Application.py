d0c3430238af30c4f905116972a5343e5845f31c
Lavan Sathiyaseelan <Lasat17@student.sdu.dk>
2021-11-13 11:48:07 +0100

    Niklas updates

--- Application.py
+++ Application.py
@@ -1,4 +1,3 @@
-
 import folium
 import geopandas as gpd
 from sentinelsat.sentinel import SentinelAPI
@@ -9,14 +8,11 @@ from rasterio.plot import show
 from rasterio.mask import mask
 from osgeo import gdal
 
-m = folium.Map([9.7174072265625, 55.024873440944816], zoom_start=11)
-m = folium.Map([10.832519531249998, 55.024873440944816], zoom_start=11)
-m = folium.Map([10.832519531249998, 55.665193184436035], zoom_start=11)
-m = folium.Map([9.7174072265625,55.665193184436035], zoom_start=11)
-m = folium.Map([9.7174072265625, 55.024873440944816], zoom_start=11)
+
+
+m = folium.Map([51.65771484375, 53.76494896075243], zoom_start=11)
 boundary = gpd.read_file(r'map.geojson')
 folium.GeoJson(boundary).add_to(m)
-m
 
 footprint = None
 for i in boundary['geometry']:
@@ -26,11 +22,18 @@ user = "group18r"
 password = "eP5NU4H82Y3BktW"
 api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
 products = api.query(footprint,
-                     date=('20200109', '20200510'), #date=('20211029', '20211029'),
-                     platformname='Sentinel-2',
-                     processinglevel='Level-2A',
-                     cloudcoverpercentage=(0, 20))
+    date = ('20210809', '20211010'),
+    platformname = 'Sentinel-2',
+    processinglevel = 'Level-2A',
+    cloudcoverpercentage = (0, 20))
 
 gdf = api.to_geodataframe(products)
 gdf_sorted = gdf.sort_values(['cloudcoverpercentage'], ascending=[True])
-gdf_sorted
+
+dinfar = gdf_sorted.values
+
+print(dinfar)
+
+api.download("9b93ff2b-9509-4bc1-ba14-a7b9e187c6e0")
+
+
